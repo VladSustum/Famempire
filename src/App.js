275c3b84bd9e13735eb72f2943b5f7b263f55ca0
Vlad <dowlabre@gmail.com>
2020-12-08 16:23:14 +0100

    fix 0.4

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Router>
      <Navbar />
      <Switch>
-       <Route path='/Famempire' exact component=
+       <Route path='/Famempire/' exact component=
        {Home} />
        <Route path='/portfolio' component={Portfolio} />
        <Route path='/onama'  component={Onama} />
